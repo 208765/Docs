5d7721651bb815a5a28bf0408aad9ac8fc9f6859
John Toral <ibuna1521@gmail.com>
2021-04-28 14:31:28 +0000

    Completed skeleton of server.py

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 from sqlite3 import Connection as SQLite3Connection
 from datetime import datetime
+
+# Needed for configuration to enforce foreign key constraints in sqlite3
 from sqlalchemy import event
 from sqlalchemy.engine import Engine
+
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
@@ -11,7 +14,19 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb.file"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 0
 
+# configure sqlite3 to enforce foreign key constraints
+
+
+@event.listens_for(Engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, SQLite3Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
+
+
 db = SQLAlchemy(app)
+now = datetime.now()
 
 # models
 
@@ -19,5 +34,73 @@ db = SQLAlchemy(app)
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
+    #
     name = db.Column(db.String(50))
     email = db.Column(db.String(50))
+    address = db.Column(db.String(200))
+    phone = db.Column(db.String(20))
+    posts = db.relationship("BlogPost", cascade="all,delete")
+
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_post"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    body = db.Column(db.String(250))
+    date = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+# routes
+
+# whenever the "/user" is appended to our URL the request is going to be routed to the following function(create_user),
+# if is a POST request
+
+
+@app.route("/user", methods=["POST"])
+def create_user():
+    pass
+
+
+@app.route("/user/descending_id", methods=["GET"])
+def get_all_users_descending():
+    pass
+
+
+@app.route("/user/ascending_id", methods=["GET"])
+def get_all_users_ascending():
+    pass
+
+
+@app.route("/user/<user_id>", methods=["GET"])
+def get_one_user(user_id):
+    pass
+
+
+@app.route("/user/<user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    pass
+
+
+@app.route("/blog_post/<user_id>", methods=["POST"])
+def create_blog_post(user_id):
+    pass
+
+
+# this has same route value and request type as above def get_one_user(user_id):
+@app.route("/user/<user_id>", methods=["GET"])
+def get_all_blog_posts(user_id):
+    pass
+
+
+@app.route("/blog_post/<blog_post_id>", methods=["GET"])
+def get_all_blog_posts(blog_post_id):
+    pass
+
+
+@app.route("/blog_post/<blog_post_id>", methods=["DELETE"])
+def delete_blog_post(blog_post_id):
+    pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
